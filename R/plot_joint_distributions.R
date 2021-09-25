03588a1104efadca78f14fba280e7d79b85c845e
Ian Moran <ian.moran11@gmail.com>
2021-09-26 07:10:28 +1000

    coverage of plot functions

--- R/plot_joint_distributions.R
+++ R/plot_joint_distributions.R
@@ -8,7 +8,7 @@ plot_joint_distributions <- function(df){
 df %>%
 select(-sim_id) %>% 
 ggplot(., aes(x = .panel_x, y = .panel_y, fill = label, colour = label)) + 
-  geom_point(shape = 16, size = 0.5, position = 'auto', alpha= .3) + 
+  geom_point(shape = 16, size = 0.5,  alpha= .3) + 
   geom_autodensity(alpha = 0.7, colour = NA, position = 'identity') + 
   facet_matrix(vars(-label), layer.diag = 2) +
   scale_fill_manual(name = NULL, values = c("red","blue","grey" )) +
--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,20 +1,11 @@
-library(devtools)
-install("../ralget")
-covr::report()
-document()
-#build()
-install()
+test_that("plots", {
 
-usethis::use_test("plots")
-
-rm(list = ls())
 library(tidyverse)
 library(tidygraph)
 library(ggraph)
-library(patchwork)
 library(magrittr)
-library(ralget)
 library(ggforce)
+library(ralget)
 library(raldag)
 library("conflicted")
 options(dplyr.print_max = 3,dplyr.print_min = 3)
@@ -28,32 +19,22 @@ x <- v("x", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) + 30, sd =  4)))
 y <- v("y", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) +  7, sd =  2)))
 m <- v("m", .f = d(~ rnorm(n = 10^4, mean = rsum(.x) + 40, sd =  2)))
 
-
 g <-
  (a * b(9) + m * b(1)) * x +
  (c * b(2) + m * b(5)) * y +
  (a * b(3) + c * b(5)) * m
 
+obs <- g %>% simulate(label = "Observational")
+do0 <- g %>% raldag::do(a = 0) %>% simulate(label = "do(a = 0)",seed = 1)
+do1 <- g %>% raldag::do(a = 1) %>% simulate(label = "do(a = 1)",seed = 1)
 
-g %>% evaluate_prepare() %>% evaluate_execute()
-
-
-g %>% evaluate_prepare() %>% evaluate_execute()
-
-
-
-h <-
- (m * b(2) + x * b(1)) * y 
+bind_rows(obs, do0, do1) %>% 
+  gather(var,value, -sim_id,-label) %>%
+  plot_distributions()
 
 
+bind_rows(obs, do0, do1) %>% plot_joint_distributions()
 
-h %>% evaluate_prepare() %>% evaluate_execute()
+ expect_true(TRUE)
 
-
-
-
-q <- (m * b(2) + x * b(1)) * y  +  ( y * b(2)*c)
-
-q %>% evaluate_prepare() %>% filter(row_number() == 1) %>% evaluate_execute()
-q %>% evaluate_prepare() %>% filter(row_number() <= 2) %>% evaluate_execute()
-q %>% evaluate_prepare() %>% filter(row_number() <= 3) %>% evaluate_execute()
+})
